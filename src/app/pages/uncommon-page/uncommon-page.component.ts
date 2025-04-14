0a70308d5a5072cc315a99f89548679d71a9c574
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2025-04-14 12:26:07 -0500

    feat: update uncommon-page with dynamic client greeting using i18nSelect pipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Edwin',
+  gender: 'male',
+  age: 27,
+  addres: 'Bogota, Colombia',
+};
+
+const client2 = {
+  name: 'Angela',
+  gender: 'female',
+  age: 28,
+  addres: 'Toronto, Canadá',
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent {}
+export default class UncommonPageComponent {
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient(): void {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+}
