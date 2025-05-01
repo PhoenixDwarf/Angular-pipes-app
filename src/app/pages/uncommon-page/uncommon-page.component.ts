538ec1439392782d76d3b9131a657be56f03f91f
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2025-05-01 11:34:45 -0500

    feat: add i18nPluralPipe support to uncommon-page for client count display

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 const client1 = {
   name: 'Edwin',
@@ -18,7 +18,7 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
@@ -36,5 +36,29 @@ export default class UncommonPageComponent {
     }
 
     this.client.set(client1);
+  }
+
+  // i18n Plural
+
+  clientsMap = signal({
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos dos clientes esperando.',
+    other: 'tenemos # clientes esperando.',
+  });
+
+  clients = signal([
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Juan',
+    'Luis',
+    'Carlos',
+    'Andres',
+    'Jorge',
+  ]);
+
+  deleteClient() {
+    this.clients.update((clients) => clients.slice(1));
   }
 }
