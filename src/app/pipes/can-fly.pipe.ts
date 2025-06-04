e7671539458ec096ff754dcfbf89e68792e15fca
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2025-06-03 21:40:42 -0500

    feat: implement custom pipes and enhance custom-page with hero data display

---
+++ src/app/pipes/can-fly.pipe.ts
@@ -0,0 +1,8 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'canFly' })
+export class CanFlyPipe implements PipeTransform {
+  transform(value: boolean): 'Puede volar' | 'No puede volar' {
+    return value ? 'Puede volar' : 'No puede volar';
+  }
+}
